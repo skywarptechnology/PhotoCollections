174c7bbc58315616908f02704ce9d592501f7c49
skyalligator <dineshbs.acc@gmail.com>
2019-02-04 17:26:37 -0600

    optimized gradle and files

--- app/src/main/java/com/sky/photocollections/support/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.sky.photocollections.support
-
-
-
-// Other keys
--- app/src/main/java/com/sky/photocollections/support/ImageDownloader.kt
+++
@@ -1,7 +0,0 @@
-package com.sky.photocollections.support
-
-/**
- * Created by skyalligator on 1/26/19.
- * 7:41 PM
- */
-class ImageDownloader
